83a0bff83fe88b4109c16a0592580013db980f80
Anton Nagy <nagya@carleton.edu>
2018-10-24 17:33:41 -0500

    Added "debug with RuntimeSearch..." option in Run menu

---
+++ PluginUI/RuntimeSearch_Plugin/src/main/java/RuntimeSearchDropdown.java
@@ -0,0 +1,8 @@
+import com.intellij.openapi.actionSystem.*;
+
+public class RuntimeSearchDropdown extends AnAction {
+    @Override
+    public void actionPerformed(AnActionEvent anActionEvent) {
+
+    }
+}
