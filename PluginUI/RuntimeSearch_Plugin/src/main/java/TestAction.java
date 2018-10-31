1b05fa55e4c2939255eadab924b37566ab7c969a
Anton Nagy <nagya@carleton.edu>
2018-10-31 16:53:05 -0500

    Added work on Tool Window

---
+++ PluginUI/RuntimeSearch_Plugin/src/main/java/TestAction.java
@@ -0,0 +1,29 @@
+import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.openapi.project.Project;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentFactory;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+
+public class TestAction implements ToolWindowFactory {
+
+    private JButton refreshToolWindowButton;
+    private JButton hideToolWindowButton;
+    private JLabel currentDate;
+    private JLabel currentTime;
+    private JLabel timeZone;
+    private JPanel myToolWindowContent;
+    private ToolWindow myToolWindow;
+
+    @Override
+    public void createToolWindowContent(Project project, ToolWindow toolWindow) {
+        myToolWindow = toolWindow;
+        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
+        Content content = contentFactory.createContent(myToolWindowContent, "Test Tool Action", false);
+        toolWindow.getContentManager().addContent(content);
+    }
+}
