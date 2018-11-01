4e3c35f40901fca2d8d8deca156694109b10b9a5
Anton Nagy <nagya@carleton.edu>
2018-11-01 12:14:14 -0500

    Fixed error with Tool Window

--- PluginUI/RuntimeSearch_Plugin/src/main/java/TestAction.java
+++ PluginUI/RuntimeSearch_Plugin/src/main/java/TestAction.java
@@ -19,13 +19,14 @@ public class TestAction implements ToolWindowFactory {
     private JPanel myToolWindowContent;
     private ToolWindow myToolWindow;
 
-    @Override
     public void createToolWindowContent(Project project, ToolWindow toolWindow) {
-//        myToolWindow = toolWindow;
-//        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
-//        Content content = contentFactory.createContent(myToolWindowContent, "Test Tool Action", false);
-//        toolWindow.getContentManager().addContent(content);
+        myToolWindow = toolWindow;
+        myToolWindowContent = new JPanel();
+        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
+        Content content = contentFactory.createContent(myToolWindowContent, "Test Tool Action", false);
+        System.out.println(myToolWindowContent);
+        toolWindow.getContentManager().addContent(content);
 
-        toolWindow.getComponent().add(new TestGUI().getPanel1());
+//        toolWindow.getComponent().add(new TestGUI().getPanel1());
     }
 }
