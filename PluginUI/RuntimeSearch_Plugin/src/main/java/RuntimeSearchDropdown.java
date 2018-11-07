c987797679d9dbf2228bd3120a67655b54c390ab
Anton Nagy <nagya@carleton.edu>
2018-11-07 17:14:26 -0600

    Added "Find Next" button & started connecting menu item with toolwindow

--- PluginUI/RuntimeSearch_Plugin/src/main/java/RuntimeSearchDropdown.java
+++ PluginUI/RuntimeSearch_Plugin/src/main/java/RuntimeSearchDropdown.java
@@ -1,8 +1,22 @@
 import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowAnchor;
+import com.intellij.openapi.wm.ToolWindowManager;
 
 public class RuntimeSearchDropdown extends AnAction {
     @Override
     public void actionPerformed(AnActionEvent anActionEvent) {
-
+//        Project project = anActionEvent.getProject();
+//        if (project == null)
+//        {
+//            return;
+//        }
+//
+//        ToolWindowManager manager = ToolWindowManager.getInstance(project);
+//        // ToolWindow window = manager.getToolWindow("Test Tool Window");
+//        ToolWindow window = manager.registerToolWindow("Test Tool Window", true, ToolWindowAnchor.BOTTOM);
+//
+//        //final ToolWindow toolWindow = window;
     }
 }
