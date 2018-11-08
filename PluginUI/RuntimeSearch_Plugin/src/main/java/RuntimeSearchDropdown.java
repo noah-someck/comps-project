0c7706c5d8a55b8a7e13f2353148703954453e9a
Thomas Scruggs <31933999+tscruggs3@users.noreply.github.com>
2018-11-07 18:13:17 -0600

    Connected RTS dropdown option with the Tool Window

--- PluginUI/RuntimeSearch_Plugin/src/main/java/RuntimeSearchDropdown.java
+++ PluginUI/RuntimeSearch_Plugin/src/main/java/RuntimeSearchDropdown.java
@@ -1,22 +1,31 @@
 import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.wm.ToolWindow;
-import com.intellij.openapi.wm.ToolWindowAnchor;
-import com.intellij.openapi.wm.ToolWindowManager;
+import com.intellij.openapi.util.ActionCallback;
+import com.intellij.openapi.wm.*;
+import com.intellij.ui.content.ContentManager;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.InputEvent;
 
 public class RuntimeSearchDropdown extends AnAction {
     @Override
     public void actionPerformed(AnActionEvent anActionEvent) {
-//        Project project = anActionEvent.getProject();
-//        if (project == null)
-//        {
-//            return;
-//        }
-//
-//        ToolWindowManager manager = ToolWindowManager.getInstance(project);
-//        // ToolWindow window = manager.getToolWindow("Test Tool Window");
-//        ToolWindow window = manager.registerToolWindow("Test Tool Window", true, ToolWindowAnchor.BOTTOM);
-//
-//        //final ToolWindow toolWindow = window;
+        Project project = anActionEvent.getProject();
+        if (project == null)
+        {
+            return;
+        }
+        ToolWindowManager manager = ToolWindowManager.getInstance(project);
+        ToolWindow window = manager.getToolWindow("Test Tool Window");
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        };
+        window.show(runnable);
     }
 }
