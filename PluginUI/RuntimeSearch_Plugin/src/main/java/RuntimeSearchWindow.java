5cc3030a23e32ebe8c6f00b430f07e2f0c56b9e6
Anton Nagy <nagya@carleton.edu>
2019-01-17 12:33:43 -0600

    added imports for last push

--- PluginUI/RuntimeSearch_Plugin/src/main/java/RuntimeSearchWindow.java
+++ PluginUI/RuntimeSearch_Plugin/src/main/java/RuntimeSearchWindow.java
@@ -26,6 +26,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.util.*;
 import java.util.List;
