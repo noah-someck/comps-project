b4b84984bea5a040720f3ec8e9102af5967fc94a
noah-someck <jamessomeck@yahoo.com>
2019-02-26 03:26:36 -0600

    Path to jar no longer hard coded

--- PluginUI/RuntimeSearch_Plugin/src/main/java/edu/carleton/cs/ASEcomps/RunWithAgentPatcher.java
+++ PluginUI/RuntimeSearch_Plugin/src/main/java/edu/carleton/cs/ASEcomps/RunWithAgentPatcher.java
@@ -4,33 +4,62 @@ import com.intellij.execution.Executor;
 import com.intellij.execution.configurations.JavaParameters;
 import com.intellij.execution.configurations.ParametersList;
 import com.intellij.execution.configurations.RunProfile;
-import com.intellij.execution.runners.ExecutionEnvironmentBuilder;
 import com.intellij.execution.runners.JavaProgramPatcher;
-import com.intellij.openapi.roots.impl.storage.ClassPathStorageUtil;
-import com.intellij.openapi.roots.ui.configuration.ClasspathEditor;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.rt.coverage.util.classFinder.ClassPathEntry;
-import com.intellij.util.PathsList;
-import com.intellij.util.lang.ClasspathCache;
-import javassist.ClassPath;
-import junit.runner.ClassPathTestCollector;
-import org.jf.dexlib2.analysis.ClassPathResolver;
 
-import java.io.IOException;
+import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
+import java.io.IOException;
+import java.util.Enumeration;
 
 public class RunWithAgentPatcher extends JavaProgramPatcher {
-    private static final String PATH_TO_AGENT_JAR = "/Users/Noah/Documents/Noah/College/Senior Year/Comps/comps-project/PluginUI/RuntimeSearch_Plugin/src/main/resources/agents/InstrumentationTest-agent.jar";
-//"C:\\Users\\T\\aseComps1\\PluginUI\\RuntimeSearch_Plugin\\src\\main\\resources\\agents\\InstrumentationTest-agent.jar";
 
     @Override
     public void patchJavaParameters(Executor executor, RunProfile configuration, JavaParameters javaParameters) {
+
         if (!executor.getId().equals("RUNTIME_SEARCH_EXECUTOR")) return;
 
+        URI uri = null;
+        try {
+            uri = new URI(getClass().getClassLoader().getResource("agents/InstrumentationTest-agent.jar").getPath());
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        String PATH_TO_AGENT_JAR = uri.getPath();
+
+        try {
+            unzip(PATH_TO_AGENT_JAR.replace("!/agents/InstrumentationTest-agent.jar", ""),
+                    PATH_TO_AGENT_JAR.replace("RuntimeSearch_Plugin-1.0-SNAPSHOT.jar!/agents/InstrumentationTest-agent.jar", ""));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        PATH_TO_AGENT_JAR = PATH_TO_AGENT_JAR.replace("RuntimeSearch_Plugin-1.0-SNAPSHOT.jar!/", "");
         ParametersList vmParametersList = javaParameters.getVMParametersList();
         vmParametersList.add( "-javaagent:" + PATH_TO_AGENT_JAR);
+    }
 
-
+    private void unzip(String jarFile, String destDir) throws IOException {
+        JarFile jar = new JarFile(jarFile);
+        Enumeration enumEntries = jar.entries();
+        while (enumEntries.hasMoreElements()) {
+            JarEntry file = (JarEntry) enumEntries.nextElement();
+            File f = new File(destDir + File.separator + file.getName());
+            if (file.isDirectory()) { // if its a directory, create it
+                f.mkdir();
+                continue;
+            }
+            java.io.InputStream is = jar.getInputStream(file); // get the input stream
+            java.io.FileOutputStream fos = new java.io.FileOutputStream(f);
+            while (is.available() > 0) {  // write contents of 'is' to 'fos'
+                fos.write(is.read());
+            }
+            fos.close();
+            is.close();
+        }
+        jar.close();
     }
 
 }
